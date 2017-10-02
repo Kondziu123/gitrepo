fb83271f750cf59a3c737cbe5a5f56b614e4bc6e
Kondziu123 <consew@wp.pl>
2017-10-02 12:36:31 +0200

    pentla.for

--- cpp/trojkat.cpp
+++ cpp/trojkat.cpp
@@ -5,16 +5,37 @@
 
 
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
 int main (int argc, char **argv)
 {
     int a, b, c;
+    int obwod = 0;
+    float p ;
+    float pole ;
     a = b = c = 0; 
-    cout <<"Podaj boki: " ;
+    cout <<"Podaj boki: " << endl;
     cin >> a >> b >> c;
-    cout << "obwód: " <<  a + b + c << endl;
+    //~if (a + b > c){
+        //~if (a + c > b){
+            //~if (b + c > a){
+                //~cout << "da się utworzyć";
+            //~}
+        //~}
+    //~}
+    if (a + b > c && a + c > b && b + c > a) {
+        obwod = a + b + c;
+        cout << "obwod: " <<  obwod << endl;
+        p = 0.5 * obwod;
+        cout << "Wsp≥ółczynnik: " << p << endl;
+        p= (a + b +c)/2
+        cout << "Pole: " << sqrt (p * (p-a) * (p-b) * (p-c)) << endl;
+    } else {
+        cout << "Nie da sie!";
+    }
+    
     
     
     
