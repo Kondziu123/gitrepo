38abb8c490be1a85f7a39b9cedbed6f6169a14e4
Kondziu123 <consew@wp.pl>
2017-10-24 10:38:33 +0200

    Tabela2

--- cpp/tabele2.cpp
+++ cpp/tabele2.cpp
@@ -7,6 +7,17 @@
 
 using namespace std;
 
+
+void ile5(int tab[], int ile){
+    int i;
+    int licznik5 =0;
+    for (int i = 0; i < ile; i++){
+        if (tab[i] % 5 == 0)
+            licznik5++;
+        }
+        cout << "ilosc liczb podzielnych przez 5: " << licznik5 << endl;
+    }
+
 void pobierzliczby(int tab[], int ile){
     int i =0;
     for (i = 0; i < ile; i++ ){
@@ -14,21 +25,13 @@ void pobierzliczby(int tab[], int ile){
         cin >> tab[i];
     }
 }
-void sumuj (int tab[], int ile){
-    int i = 0;
-    int suma = 0;
-    for (i = 0; i < ile; i++){
-        suma += tab[i];
-        }
-        cout << "Sumuj liczby: " << suma << endl;     
-}
-void najmniejsza int (int tab[], int ile){
+void najmniejsza (int tab[], int ile){
     int mini = tab [0];
     for (int i = 0; i < mini; i++){
         if (mini > tab[1])
-        mini += tab [1;]
+        mini += tab [1];
         }
-        cout << "najmniejsza liczba: " << mini <endl;
+        cout << "najmniejsza liczba: " << mini << endl;
     ; // funkcja znajduje i drukuje najmniejsza
       // liczba z tabeli
 }
@@ -43,9 +46,8 @@ int main(int argc, char **argv)
 	int liczby[rozmiar];
     
     pobierzliczby (liczby, rozmiar);
-    sumuj (liczby, rozmiar);
     najmniejsza (liczby, rozmiar);
-    
+    ile5 (liczby, rozmiar);
 	return 0;
 }
 
