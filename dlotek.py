856a63b0f348519ee3fa1b5bd35e160b4521abed
Kondziu123 <consew@wp.pl>
2017-11-10 08:46:27 +0100

    dlotek

--- dlotek.py
+++ dlotek.py
@@ -4,18 +4,45 @@
 import random
 
 
+def losuj(ileliczb, maksliczb):
+    liczby = []
+    ile = 0
+    while ile < ileliczb:
+        liczba = random.randint(0, maksliczb)
+        if liczby.count(liczba) == 0:
+            liczby.append(liczba)
+            ile += 1
+
+    print(liczby)
+    return liczby
+
+
+def pobierz_typy(ileliczb):
+    typy = set()
+    # for i in range(ileliczb):
+    ile = 0
+    while ile < ileliczb:
+        typ = int(input('Podaj typ: '))
+        if typ not in typy:
+            typy.add(typ)
+            ile += 1
+
+    # print(typy)
+    return typy
+
+
 def main(args):
     ileliczb = int(input('Ile liczb chce zgadywać'))
     maksliczb = int(input('Maksymalna losowana liczba:'))
 
-    liczby = []
+    while ileliczb > maksliczb or ileliczb < 1:
+        ileliczb = int(input('Ile iczb chcesz zgadnąć z %s liczb' % maksliczb))
 
-    for i in range(ileliczb):
-        liczba = (random.randint(0, maksliczba))
-        if liczby.count(liczba) == 0:
-           liczby.append(liczba)
+    liczby = losuj(ileliczb, maksliczb)
+    typy = pobierz_typy(ileliczb)
 
-    print(liczby)
+    trafione = set(liczby) & typy
+    print('Trafiłeś', len(trafione), 'liczb')
     return 0
 
 
