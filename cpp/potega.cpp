02cb0039fa7ba956ba2c43525fd3f858d8577283
Kondziu123 <consew@wp.pl>
2017-11-14 10:36:38 +0100

    Silnia

---
+++ cpp/potega.cpp
@@ -0,0 +1,35 @@
+/*
+ * potega.cpp
+ * a0 = 1
+ * a1 = a 
+ * an = a*....*a (n - czynników) dla n zaw. N+ - {1} 
+ */
+
+
+#include <iostream>
+
+
+using namespace std;
+
+float potega_it(float x, int n) {
+    float wynik = 1;
+    for(int i = 0; i < n; i++) {
+        cout << i << endl;
+        wynik = wynik * x;
+    }
+    return wynik;    
+} 
+
+int main(int argc, char **argv)
+{
+    float x= 0;
+    int n = 0;
+	//pobierz od użytkownika podstawę i wykładnik
+    cout << "Podstawa: " << endl;
+    cin >> x;
+    cout << "Wykładnik: " << endl;
+    cin >> n;
+    cout << "Potęga: " << potega_it(x, n) << endl;
+	return 0;
+}
+
---
+++ python/silnia.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# silnia.py
+# n! = 1 dla n=(0, 1)
+# n! = 1 * .... * n dla N+ - {1}
+
+def silnia_it(n):
+    wynik = 1
+    for i in range(2, n + 1):
+        wynik = wynik * i 
+    return wynik
+    
+
+def main(args):
+    """Funkcja głównna"""
+    # pobierz od użytkownikaliczbe naturalna
+    # i przypisz ja do zmiennej
+    # wywołaj funkcje silnia it() z odpowiednim argumentem
+    n = int(input('Podaj liczbe: '))
+    print(silnia_it(n))
+    return 0
+
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
