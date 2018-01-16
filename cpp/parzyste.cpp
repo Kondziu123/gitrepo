0d3a5d4e822368d889a1c97127b459e25ad50ffc
Kondziu123 <consew@wp.pl>
2018-01-16 10:36:05 +0100

    inf

---
+++ cpp/parzyste.cpp
@@ -0,0 +1,18 @@
+/*
+ * parzyste.cpp 
+ */
+
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+	int a;
+    cout << "Podaj wartość liczby a: ";
+    cin >> a;
+    for(int a = 0)
+	return 0;
+}
+
