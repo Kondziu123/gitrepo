614cb59ab14aa746a3214e6f5cab25e84dc9faf4
Kondziu123 <consew@wp.pl>
2018-01-22 12:34:34 +0100

    dom

---
+++ cpp/Sewera_alg1.cpp
@@ -0,0 +1,22 @@
+/*
+ * Sewera_alg1.cpp
+ */
+
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+	int a;
+    cout << "Podaj wartość liczby a: ";
+    for(int i = 1; i < 100; i ++);
+        i = 2;
+        if( a = i);
+            cout <<"Liczba jest parzysta";
+        else;
+            i = i + 2;
+    return 0;
+}
+
